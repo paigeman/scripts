60dd4288e5522c9a76f5b0c2db3760f3fe111807
paigeman <53284808+paigeman@users.noreply.github.com>
2022-11-14 00:05:13 +0800

    update auto-start-Clash-for-Windows.sh

--- auto-start-Clash-for-Windows.sh
+++ auto-start-Clash-for-Windows.sh
@@ -9,11 +9,38 @@ then
     echo "directory does not exist"
     exit 1
 fi
+# FIXME:path occurs two slash
 if ! test -e $CLASH_DIR/cfw
 then
     echo "directory is not right, please check"
     exit 1
 fi
-# generate auto start script
 
+# remove cfw service
+cd /etc/init.d
+update-rc.d cfw remove
+
+# generate auto start script
+cat > cfw.sh << END_TEXT
+#!/bin/sh
+
+
+### BEGIN INIT INFO
+# Required-Start:    $all
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+### END INIT INFO
+
+LOG_DIR=/var/log/cfw
+$CLASH_DIR/cfw
+if ! test -e \$LOG_DIR
+then
+  mkdir \$LOG_DIR
+fi
+echo "start successfully" >> \$LOG_DIR/\$(date +"%Y-%m-%d-%H-%M-%S").txt
+END_TEXT
+    
 # use update-rc service to achieve it
+chmod +x cfw.sh
+# FIXME: update-rc.d: error: unable to read /etc/init.d/cfw
+update-rc.d cfw defaults
