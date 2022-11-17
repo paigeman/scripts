337c7ee38a54575398b1e32445a9fd89b8a9b20c
paigeman <53284808+paigeman@users.noreply.github.com>
2022-11-17 23:45:34 +0800

    create Clash-for-Windows-install.sh

---
+++ Clash-for-Windows-install.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+RELEASE_API=https://api.github.com/repos/Fndroid/clash_for_windows_pkg/releases
+PAGE_SIZE=5
+
+# process json
+
+# use ansi control escape code or normal situation to print the result of the command
+
+# download the selected version
+curl -O 
+
+# decompress file
+
+# install it
