2ba8740acee30b9f640490b343f99a8ea7919f6c
paigeman <53284808+paigeman@users.noreply.github.com>
2022-11-19 23:46:09 +0800

    #3 finish get_data function

--- Clash-for-Windows-install.py
+++ Clash-for-Windows-install.py
@@ -2,9 +2,13 @@
 
 import json
 import requests
+from tabulate import tabulate
 
 REALEASE_API = "https://api.github.com/repos/Fndroid/clash_for_windows_pkg/releases"
 PAGE_SIZE = 5
 
-def get_data(page_number:1):
+def get_data(page_number=1):
     params = {"per_page": PAGE_SIZE, "page": page_number}
+    r = requests.get(REALEASE_API, params)
+    return r.json()
+
