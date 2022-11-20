d560792db9f90871f644ca7fadd311800bec659f
paigeman <53284808+paigeman@users.noreply.github.com>
2022-11-20 23:53:36 +0800

    #3 finish print_table function

--- Clash-for-Windows-install.py
+++ Clash-for-Windows-install.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 
-import json
 import requests
 from tabulate import tabulate
 
-REALEASE_API = "https://api.github.com/repos/Fndroid/clash_for_windows_pkg/releases"
 PAGE_SIZE = 5
 
 def get_data(page_number=1):
+    release_api = "https://api.github.com/repos/Fndroid/clash_for_windows_pkg/releases"
     params = {"per_page": PAGE_SIZE, "page": page_number}
-    r = requests.get(REALEASE_API, params)
+    r = requests.get(release_api, params)
     return r.json()
 
+def  print_table(data):
+    headers = ["No", "Version"]
+    version_data = [(i, e['name']) for i,e in enumerate(data)]
+    print(tabulate(version_data, headers=headers, tablefmt="pretty"))
+
+def download(data, index):
+    
+    pass
+
+print_table(get_data())
